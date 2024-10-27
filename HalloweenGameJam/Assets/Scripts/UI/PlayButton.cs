74ff0fe2d0e309d0a7af44a54ecaf5cc994bdf12
Elliot <elliot.calais1@gmail.com>
2024-10-27 18:17:42 +0000

    Main menu
    Added a main menu scene with a play button which increments the scene number (moves to main scene) and quit button which only works in final build

---
+++ HalloweenGameJam/Assets/Scripts/UI/PlayButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+
+    // when play button is pressed, the scene number will be incremented
+    public void PressPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
