cbf682fabc4f2ebaf572515bd8ee81b530ff84c1
YXPhoon1220 <34766331+YXPhoon1220@users.noreply.github.com>
2024-10-29 00:21:04 +0000

    Added player animations

---
+++ HalloweenGameJam/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    private Rigidbody2D rb;
+
+    // Animator initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        animator.SetInteger("X Axis", -1);
+        animator.SetInteger("Y Axis", 0);
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.velocity.magnitude > 0) {
+            animator.SetBool("Is Moving", true);
+
+        } else {
+            animator.SetBool("Is Moving", false);
+        }
+    }
+
+        void Update()
+    {
+        int x = (int)Math.Round(Input.GetAxisRaw("Horizontal"));
+        int y = (int)Math.Round(Input.GetAxisRaw("Vertical"));
+
+        // Don't update the variables to both be 0 when idling so that the animator remembers the last orientation
+        if (x != 0 || y != 0)
+        {
+            animator.SetInteger("X Axis", x);
+            animator.SetInteger("Y Axis", y);
+        }
+        
+
+    }
+}
